3b89c85d49e84950602a2a9d1bb135fd3ff5aa45
karli sensibello <45071344+Sensibello@users.noreply.github.com>
2018-12-11 13:03:29 -0500

    Update oop.js

--- Scripts/oop.js
+++ Scripts/oop.js
@@ -1,13 +1,3 @@
-/*
- * *****
-mario
-mario javascript project
-
-Hi everyone I followed a tutorial by taniarascia.com and florian rappl to build a game in 'mario style' for fun.
--sensibello.com
- * *****
- */
-
 
 var reflection = {};
 
